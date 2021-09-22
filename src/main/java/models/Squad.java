cdd6c37fd3b46afa6cc9c63cceb26f00cfcb6033
kamotho-njiru <samuel.kamotho@student.moringaschool.com>
2021-09-22 07:49:06 +0300

    created squad test file to test itd bdd

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -5,4 +5,15 @@ public class Squad {
     private String name;
     private String duty;
 
+    public char getMaxSize(){return maxsize;}
+    public String getName() {return name;}
+    private String duty (){return duty;}
+
+    public Squad(char maxsize, String name, String duty){
+        this.maxsize= maxsize;
+        this.name= name;
+        this.duty= duty;
+
+    }
+
 }
