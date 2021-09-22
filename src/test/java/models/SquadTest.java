1a81a29a7caea42102e88d3c16e2da646acaf67f
kamotho-njiru <samuel.kamotho@student.moringaschool.com>
2021-09-22 08:24:16 +0300

    added red green workflow to the squad test

--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -1,4 +1,26 @@
 package models;
 
+import org.junit.Test;
+
 public class SquadTest {
+    @Test
+    public void newSquad_getSquad_char(){
+        Squad newSquad= Squad.insertNewSquad();
+        assertEquals(4, newSquad.getMaxSize());
+    }
+
+    @Test
+    public void newSquad_getName_String(){
+        Squad newSquad= Squad.insertNewSquad();
+        assertEquals("Titans", newSquad.getName());
+    }
+
+    @Test
+    public void newSquad_getSquad_String(){
+        Squad newSquad= Squad.insertNewSquad();
+        assertEquals("Protect and Serve", newSquad.getDuty());
+
+    }
+
+
 }
